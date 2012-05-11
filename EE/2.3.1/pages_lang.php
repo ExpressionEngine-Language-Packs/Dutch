c383c524459027a98286430ba960f66b7b59b13e
Thijs Witteveen <onderhoud@am-impact.nl>
2012-05-11 14:43:52 +0200

    vertaling EE 2.3.1

---
+++ EE/2.3.1/pages_lang.php
@@ -0,0 +1,137 @@
+<?php
+$lang = array(
+
+
+'pages_module_name' => 
+'Pagina\'s',
+
+'pages_module_description' => 
+'Gebruikt Weblog Berichten om Statische pagina&#39;s te maken',
+
+'pages_homepage' => 
+'Pagina\'s Homepage',
+
+'page' => 
+'Pagina',
+
+'pages_uri' => 
+'Pagina URI',
+
+'no_pages' => 
+'Er bestaan nog geen pagina\'s',
+
+'create_page' => 
+'Maak nieuwe pagina',
+
+'page_name' => 
+'Pagina Naam',
+
+'edit_page' => 
+'Wijzig Pagina',
+
+'view_page' => 
+'Bekijk Pagina',
+
+'page_settings' => 
+'Pagina Instellingen',
+
+'none' => 
+'Geen',
+
+'template' => 
+'Template',
+
+'parent_page' => 
+'Hoofd Pagina',
+
+'channel_entry' => 
+'Channel Bericht',
+
+'choose_entry' => 
+'Kies Bericht',
+
+'pages_delete_confirm' => 
+'Verwijder Pages',
+
+'pages_delete_question' => 
+'Weet u zeker dat u de geselecteerde pagina URI(s) wil verwijderen?<br /><em>Let Op: Een Page verwijderen verwijdert alleen de URL. Het bericht wordt niet verwijderd.</em>',
+
+'page_deleted' => 
+'Page Verwijderd',
+
+'pages_deleted' => 
+'Pages Verwijderd',
+
+'create_entry' => 
+'Maak Bericht',
+
+'choose_template' => 
+'Kies Template voor tonen van Page',
+
+'invalid_page_name' => 
+'Ongeldige Page Naam Ingevoerd',
+
+'invalid_template' => 
+'U moet een geldig template invoeren om deze page te tonen.',
+
+'page_created' => 
+'Pagina Aangemaakt',
+
+'page_updated' => 
+'Pagina geupdate',
+
+'invalid_page_uri' => 
+'Ongeldige pagina URI',
+
+'invalid_page_num_segs' => 
+'U heeft het maximaal aantal URI segementen bereikt in de pagina URL',
+
+'pages_configuration' => 
+'Module Configuratie',
+
+'preference_name' => 
+'Voorkeur Naam',
+
+'preference_value' => 
+'Voorkeur Waarde',
+
+'default_template' => 
+'Standaard Template',
+
+'default_for_page_creation' => 
+'Standaard channel voor &#39;Maak Nieuwe Page&#39; Tab',
+
+'no_default' => 
+'Geen Standaard',
+
+'configuration_updated' => 
+'Configuratie Gewijzigd',
+
+'duplicate_page_uri' => 
+'Dupliceer pagina URI',
+
+'pages_display_on_homepage' => 
+'Laten zien van URIs op de Module Homepage',
+
+'nested' => 
+'Genest',
+
+'not_nested' => 
+'Niet Genest',
+
+'preference' => 
+'Voorkeur',
+
+'setting' => 
+'Instelling',
+
+'example_uri' => 
+'/example/pages/uri/',
+
+'translate' => 
+'Update',
+
+''=>''
+);
+
+// End of File
