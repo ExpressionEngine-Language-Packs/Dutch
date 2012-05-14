120f6653a64f0b1121d934010d42004a81c64829
Thijs Witteveen <onderhoud@am-impact.nl>
2012-05-14 11:36:45 +0200

    Wygwam 2.5.0 original language file + Dutch translation

---
+++ Addons/Wygwam/2.5.0/lang.wygwam.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once PATH_THIRD.'wygwam/config.php';
+
+
+$lang = array(
+
+// -------------------------------------------
+//  Module CP
+// -------------------------------------------
+
+'wygwam_module_name' => WYGWAM_NAME,
+'wygwam_module_description' => WYGWAM_DESC,
+
+'wygwam_no_module' => 'Je hebt de Wygwam module nog niet geïnstalleerd.',
+
+// Pages
+'wygwam_settings' => 'Instellingen',
+'wygwam_configs' => 'Editor Configuraties',
+
+// Settings
+'wygwam_license_key' => 'License Key',
+'wygwam_file_browser' => 'Bestandsbeheer',
+'wygwam_file_browser_desc' => 'Welke bestandsbeheer moet er gebruikt worden voor het zoeken van afbeeldingen en bestanden in Wygwam velden?',
+
+// Configs
+'wygwam_clone' => 'Kopieer',
+'wygwam_no_configs' => 'Er zijn op dit moment geen configuraties.',
+
+// Edit Config
+'wygwam_create_config' => 'Configuratie toevoegen',
+'wygwam_edit_config' => 'Configuratie wijzigen',
+'wygwam_config_settings' => 'Configuratie instellingen',
+'wygwam_config_name' => 'Configuratie Naam',
+'wygwam_toolbar' => 'Knoppenbalk configureren',
+'wygwam_toolbar_help1' => 'Selecteer knoppengroepen door ze te slepen<br />van de onderste balk<br />naar de voorbeeld knoppenbalk.',
+'wygwam_toolbar_help2' => 'Je kunt ook nog losse knoppen<br />aan/uitzetten door<br />erop te klikken.',
+'wygwam_height' => 'Hoogte',
+'wygwam_resizable' => 'Aanpasbaar',
+'wygwam_css' => 'CSS File',
+'wygwam_css_desc' => 'Url naar de CSS file die je wilt gebruiken voor de inhoud van het veld.',
+'wygwam_upload_dir' => 'Upload Directory',
+'wygwam_advanced' => 'Geavanceerde Instellingen',
+'wygwam_config_saved' => 'Deze configuratie is succesvol opgeslagen.',
+
+// Delete Config
+'wygwam_delete_config' => 'Configuratie verwijderen',
+'wygwam_delete_config_confirm' => 'Weet je zeker dat je deze configuratie wilt verwijderen?',
+'wygwam_config_deleted' => 'Je configuratie is succesvol verwijderd.',
+
+// -------------------------------------------
+//  Field Settings
+// -------------------------------------------
+
+'wygwam_convert' => 'Converteer oude items?',
+'wygwam_convert_desc' => 'Wygwam kan de content van oude items converteren naar HTML.',
+'wygwam_editor_config' => 'Editor Configuratie',
+'wygwam_edit_configs' => 'Configuratie wijzigen',
+'wygwam_defer' => 'CKEditor niet direct laden?',
+'wygwam_defer_desc' => 'Wygwam zal dan pas geinitialiseerd worden op het moment dat er op het veld geklikt wordt.',
+
+// -------------------------------------------
+//  Fields
+// -------------------------------------------
+
+'wygwam_site_page' => 'Site Pagina',
+'wygwam_site_template' => 'Site Template',
+
+''=>''
+);
